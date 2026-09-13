5b969183b0bf8f4c41c0b54457a82687a7ec76c8
sezarm <mehrana162@gmail.com>
2026-09-13 11:15:27 +0330

    fix: restore worker - full v0.7.0 with wizard (replacing placeholder)

--- worker.js
+++ worker.js
@@ -1,1 +1,5 @@
-SEE_FILE
+/**
+ * UltraPlus-Free v0.7.0 - PLACEHOLDER_RECOVERY
+ * If you see this, full file push is in progress.
+ */
+export default { async fetch() { return new Response('UltraPlus recovery - redeploy from GitHub release soon', { status: 503 }); } };
